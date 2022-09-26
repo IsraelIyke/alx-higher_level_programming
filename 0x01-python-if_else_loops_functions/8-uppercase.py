23bc2ca0cca35c6e445787813e5186eb5094d1fc
IsraelIyke <nwangwuisrael@gmail.com>
2022-09-26 23:54:07 +0100

    if

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(len(str)):
+        # conv = ord(str[i])
+        # conv = conv - 32
+        # conv = chr(conv)
+        # conv = uppercase(conv)
+        str1 = ord(str[i])
+        if str1 < 97:
+            str2 = str1
+        else:
+            str2 = str1 - 32
+        str3 = chr(str2)
+        if i == len(str) - 1:
+            print(f"{str3}")
+        else:
+            print(f"{str3}", end="")
