e3fe8784b297cf1a2f9a4634765a5fac4bbac842
IsraelIyke <nwangwuisrael@gmail.com>
2022-09-26 23:22:41 +0100

    if

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,18 +1,10 @@
 #!/usr/bin/env python3
-# def islower(c):
-#     conv = ord(c)
-#     for i in range(97, 123):
-#         if conv == i:
-#             return True
-#         else:
-#             return F
 def islower(c):
     for i in range(97, 123):
         if ord(c) == i:
             return True
         else:
             return False
-        # return i == ord(c)
 
 
 islower("g")
