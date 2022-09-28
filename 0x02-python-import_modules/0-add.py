20a3369838fe30296c1bcfb1a1298655da96a034
IsraelIyke <nwangwuisrael@gmail.com>
2022-09-28 21:43:00 +0100

    mod

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,12 +1,10 @@
-#!/usr/bin/python
-from add_0 import add
-
+#!/usr/bin/python3
 if __name__ == "__main__":
-    import add_0
+    import sys
 
-    add(int(add_0.argv[1]))
+    add(int(sys.argv[1]))
 
-    a = 1
-    b = 2
-    func = add(a, b)
-    print("{} + {} = {}".format(a, b, func))
+a = 1
+b = 2
+func = add(a, b)
+print("{} + {} = {}".format(a, b, func))
