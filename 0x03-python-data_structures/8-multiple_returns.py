1cbb94ddde81b808e0ec71af6572215cbc054dcb
IsraelIyke <nwangwuisrael@gmail.com>
2022-10-04 00:34:25 +0100

    list

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    str1 = sentence
-    str2 = len(str1)
-    str1.split()
-    return str2, str1[0]
+    if len(sentence) < 1:
+        length = len(sentence)
+        return length, None
+    else:
+        str1 = sentence
+        str2 = len(str1)
+        str1.split()
+        return str2, str1[0]
