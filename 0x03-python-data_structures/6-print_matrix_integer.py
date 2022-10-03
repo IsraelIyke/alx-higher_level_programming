e7738b7611afe6cc4d7350eda2ef03721e04a7a2
IsraelIyke <nwangwuisrael@gmail.com>
2022-10-03 02:17:59 +0100

    list

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if len(matrix) == 0:
-        print("")
+        print(" ")
     else:
         for i in range(len(matrix)):
             for j in range(len(matrix[i])):
