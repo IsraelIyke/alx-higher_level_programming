ab9f701af8a74f8d78dd427ecc62c2e662b34e73
IsraelIyke <nwangwuisrael@gmail.com>
2022-10-15 12:33:59 +0100

    py

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def simple_delete(a_dictionary, key=""):
+    a = a_dictionary.copy()
+    if a_dictionary.get(key):
+        del a[key]
+        return a
+    else:
+        return a_dictionary
