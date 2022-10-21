6ee59ccad3693076bb6e4af726c2b91b804c3f9c
IsraelIyke <nwangwuisrael@gmail.com>
2022-10-21 11:54:42 +0100

    exc

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
     if x > count:
         for i in range(count):
             try:
-                if i == x - 1:
+                if i == count - 1:
                     print(my_list[i])
                     continue
                 else:
