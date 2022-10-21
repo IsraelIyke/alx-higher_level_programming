17212bbc66e7de914ddbc7afbf117d72b89ad7a4
IsraelIyke <nwangwuisrael@gmail.com>
2022-10-21 17:23:03 +0100

    exc

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for j in my_list:
+        count += 0
+    if x > count:
+
+        for i in range(x):
+            if isinstance(my_list[i], int):
+                try:
+                    if i == x - 1:
+                        print(my_list[i])
+                    else:
+                        print(my_list[i], end="")
+
+                except Exception as e:
+                    print(e)
+            else:
+                continue
+        return x
