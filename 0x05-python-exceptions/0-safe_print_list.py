a8a120c2be7f4ce15594150dde5d64651ef356c5
IsraelIyke <nwangwuisrael@gmail.com>
2022-10-21 11:52:12 +0100

    exc

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,30 @@
 #!/usr/bin/python3
+from itertools import count
+
+
 def safe_print_list(my_list=[], x=0):
-    for i in range(x):
-        try:
-            if i == x - 1:
-                print(my_list[i])
-                continue
-            else:
-                print(my_list[i], end="")
-        except Exception as e:
-            print(e)
-    return x
+    count = 0
+    for j in my_list:
+        count += 1
+    if x > count:
+        for i in range(count):
+            try:
+                if i == x - 1:
+                    print(my_list[i])
+                    continue
+                else:
+                    print(my_list[i], end="")
+            except Exception as e:
+                print("there is an error here")
+        return count
+    else:
+        for i in range(x):
+            try:
+                if i == x - 1:
+                    print(my_list[i])
+                    continue
+                else:
+                    print(my_list[i], end="")
+            except Exception as e:
+                print("there is an error here")
+        return x
