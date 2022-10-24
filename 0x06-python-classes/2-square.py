b9847d2f1114f683bf850c69a670a814af6c3d7a
IsraelIyke <nwangwuisrael@gmail.com>
2022-10-24 17:23:02 +0100

    class

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""A square module"""
+
+
+class Square:
+    """A square class"""
+
+    def __init__(self, size=0):
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            if type(size) is not int:
+                raise TypeError("size must be an integer")
+            else:
+                try:
+                    self.__size = size
+                except TypeError as e:
+                    print(e)
