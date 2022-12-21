9ca0c19d34cc800bfeea8a986cfebd0cff315a99
IsraelIyke <nwangwuisrael@gmail.com>
2022-12-21 23:47:42 +0100

    ok

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,14 @@
+const arr = process.argv;
+const newArray = [];
+if (arr.length < 3 || arr.length === 3) {
+  console.log(0);
+} else {
+  for (let i = 2; i < arr.length; i++) {
+    newArray.push(Math.floor(Number(arr[i])));
+  }
+  const max = Math.max(...newArray);
+  const largest = newArray.indexOf(max);
+  newArray.pop(largest);
+  const maxTwo = Math.max(...newArray);
+  console.log(maxTwo);
+}
